44e2b5ce8e782bf60a7ba9d38a85a907962afa83
Anil Kumar Nayak <anilkumarnayak648@gmail.com>
2026-05-27 23:20:16 +0530

    some changes

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -71,6 +71,7 @@ const AdminDashboard = () => {
     return (
         <div className="admin-dashboard">
             {/* Mobile Menu Toggle */}
+            <h1>hello</h1>
             <button 
                 className="mobile-menu-toggle" 
                 id="mobile-menu-toggle"
